b4bd9ea1ac143aa89b94683465857d794c65932a
Mohammad Shihan <mohammadshihan2002@gmail.com>
2026-03-25 14:57:40 +0000

    Fix social link placement and hide removed icons

--- sources/data/social.js
+++ sources/data/social.js
@@ -1,7 +1,7 @@
 export default [
-    { name: 'Mail', url: 'mailto:mohammad.shihan@outlook.com', align: 'right' },
-    { name: 'Twitch', url: 'https://www.twitch.tv/bruno_simon_dev', align: 'right' },
-    { name: 'GitHub', url: 'https://github.com/shihan220', align: 'right' },
-    { name: 'LinkedIn', url: 'https://www.linkedin.com/in/mohammad-shihan/', align: 'left' },
-    { name: 'Discord', modal: 'discord', align: 'left' },
+    { name: 'Mail', url: 'mailto:mohammad.shihan@outlook.com', align: 'right', objectName: 'mail' },
+    { name: 'Twitch', url: 'https://www.twitch.tv/bruno_simon_dev', align: 'right', objectName: 'twitch' },
+    { name: 'GitHub', url: 'https://github.com/shihan220', align: 'right', objectName: 'gitHub' },
+    { name: 'LinkedIn', url: 'https://www.linkedin.com/in/mohammad-shihan/', align: 'left', objectName: 'linkedIn' },
+    { name: 'Discord', modal: 'discord', align: 'left', objectName: 'discord' },
 ]
